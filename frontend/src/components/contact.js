2b5fb9a0012b1f59991cd1ffb6105cde136fca8c
Magda <>
2023-06-25 07:34:13 -0400

    contact  section

--- frontend/src/components/contact.js
+++ frontend/src/components/contact.js
@@ -1,10 +1,10 @@
 function Contact() {
 	return (
-		<section className="px-[304px] py-[48px]">
-			<form className="flex flex-col justify-between">
-				<div className="flex flex-row justify-center items-center">
-					<div className="flex-1 mr-24">
-						<div className="flex flex-col">
+		<section className="flex flex-col justify-center py-[48px]">
+			<form className="flex flex-col justify-between items-center">
+				<div className="flex flex-col justify-between items-center">
+					<div className="flex flex-row ">
+						<div className="flex flex-col mr-24 flex-1">
 							<label for="username" className="font-bold text-base font-Nunito text-dark-blue mb-4">
 								First Name:
 							</label>
@@ -17,22 +17,8 @@ function Contact() {
 								className="border-2 border-grayish rounded mb-8 text-base px-4 py-2 outline-none text-very-dark-blue focus:border-light-blue"
 							/>
 						</div>
-						<div className="flex flex-col">
-							<label for="emaul" className="font-bold text-base font-Nunito text-dark-blue mb-4">
-								Email:
-							</label>
-							<input
-								type="email"
-								id="email"
-								name="email"
-								aria-required="true"
-								aria-label="email"
-								className="border-2 border-grayish rounded mb-8 text-base px-4 py-2 outline-none text-very-dark-blue focus:border-light-blue"
-							/>
-						</div>
-					</div>
-					<div className="flex-1">
-						<div className="flex flex-col">
+
+						<div className="flex flex-col flex-1">
 							<label for="lastname" className="font-bold text-base font-Nunito text-dark-blue mb-4">
 								Last Name:
 							</label>
@@ -45,6 +31,22 @@ function Contact() {
 								className="border-2 border-grayish rounded mb-8 text-base px-4 py-2 outline-none text-very-dark-blue focus:border-light-blue"
 							/>
 						</div>
+					</div>
+					<div className="flex flex-row">
+						<div className="flex flex-col mr-24">
+							<label for="email" className="font-bold text-base font-Nunito text-dark-blue mb-4">
+								Email:
+							</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								aria-required="true"
+								aria-label="email"
+								className="border-2 border-grayish rounded mb-8 text-base px-4 py-2 outline-none text-very-dark-blue focus:border-light-blue"
+							/>
+						</div>
+
 						<div className="flex flex-col">
 							<label for="phone" className="font-bold text-base font-Nunito text-dark-blue mb-4">
 								Phone number:
@@ -61,7 +63,7 @@ function Contact() {
 						</div>
 					</div>
 				</div>
-				<div className="flex flex-col">
+				<div className="flex flex-col w-[518px] justify-center">
 					<label for="msg" className="font-bold text-base font-Nunito text-dark-blue mb-4">
 						Message:
 					</label>
@@ -73,7 +75,7 @@ function Contact() {
 						className="h-[178px] border-2 border-grayish rounded mb-8 text-base px-4 py-2 outline-none text-very-dark-blue focus:border-light-blue"
 					/>
 				</div>
-				<div className="flex flex-col items-end mt-16">
+				<div className="mt-16 w-[518px] flex flex-row justify-end ">
 					<button
 						type="submit"
 						className="font-bold text-sm font-Nunito py-6 px-10 bg-pink uppercase text-white rounded-[10px] hover:bg-light-pink"
