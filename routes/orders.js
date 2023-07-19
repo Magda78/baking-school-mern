301352d2564e95932140085827dbcd01bb216381
Magda <>
2023-07-19 18:13:48 -0400

    routes validation

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const orders = require('../controllers/orders-controllers');
+const { check } = require('express-validator');
 
 router.get('/orders', orders.getOrders);
-router.post('/orders', orders.createOrder);
-router.get('/orders/user/:id', orders.getOrdersByUserId)
+router.post('/orders', [ check('name').not().isEmpty(), check('timeOfTheDay').not().isEmpty() ], orders.createOrder);
+router.get('/orders/user/:id', orders.getOrdersByUserId);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../controllers/users-controllers');
+const { check } = require('express-validator');
 
 router.get('/users', Users.getUsers);
-router.post('/users/signUp',Users.signUp)
+router.post('/users/signUp',[ check('firstName').not().isEmpty(), check('lastName').not().isEmpty(),check('email').normalizeEmail().isEmail(),check('password').isLength({min:6}) ],Users.signUp)
 router.post('/users/login',Users.login)
 module.exports = router;
