f5b206536264baec6602c781653a075d2944f8a6
Magda <>
2023-08-17 19:00:33 -0400

    cart setup

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -8,7 +8,7 @@ function Cart({ setCart, cart }) {
 	const basketItems = useSelector(selectItems);
 	return (
 		<section
-			className={`w-[35%] h-screen bg-white p-10 flex flex-col fixed transition-all duration-500 ease-in-out ${cart
+			className={`w-[40%] h-screen bg-white p-10 flex flex-col fixed transition-all duration-500 ease-in-out ${cart
 				? 'right-0 '
 				: '-right-full'} `}
 		>
@@ -23,19 +23,19 @@ function Cart({ setCart, cart }) {
 					<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue  ">Your cart is empty</h2>
 				</div>
 			) : (
-				<div className="grid-flow-row">
-					<div className="flex items-center space-x-12 mb-8 justify-between border-b-2 border-pink py-4">
+				<div className="grid grid-rows-[50px,1fr,50px]">
+					<div className="grid grid-cols-5 auto-col-max border-b-2 border-pink py-4 text-center gap-2">
 						<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue">Program</h2>
 						<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue">Qty.</h2>
 						<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue">Date</h2>
 						<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue">Price</h2>
 						<h2 />
 					</div>
-					<div>{basketItems.map((item) => <CartItem item={item} />)}</div>
-                    <div className='mt-8 flex items-center justify-between border-t-2 border-pink py-4'>
-                        <h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">Total:</h2>
-                        <h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">${total}</h2>
-                    </div>
+					<div >{basketItems.map((item) => <CartItem item={item} />)}</div>
+					<div className="flex items-center justify-between border-t-2 border-pink py-4">
+						<h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">Total:</h2>
+						<h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">${total}</h2>
+					</div>
 				</div>
 			)}
 		</section>
--- frontend/src/components/cartItem.js
+++ frontend/src/components/cartItem.js
@@ -4,11 +4,11 @@ function CartItem({ item }) {
 	const { program, students, date, price } = item;
 	return (
 		<section>
-			<div className="flex items-center justify-between py-4">
-				<h2 className="w-[10px] font-normal text-xs font-Nunito uppercase text-dark-blue">{program}</h2>
-				<h2 className=" w-[10px] font-normal text-xs font-Nunito uppercase text-dark-blue">{students}</h2>
-				<h2 className=" w-[10px] font-normal text-xs font-Nunito uppercase text-dark-blue">{date}</h2>
-				<h2 className="w-[10px] font-normal text-xs font-Nunito uppercase text-dark-blue">${price}</h2>
+			<div className="grid grid-cols-5 text-center py-4 gap-2">
+				<h2 className="font-normal text-xs font-Nunito uppercase text-dark-blue">{program}</h2>
+				<h2 className=" font-normal text-xs font-Nunito uppercase text-dark-blue">{students}</h2>
+				<h2 className=" font-normal text-xs font-Nunito uppercase text-dark-blue">{date}</h2>
+				<h2 className=" font-normal text-xs font-Nunito uppercase text-dark-blue">${price}</h2>
 				<DeleteForeverOutlinedIcon className="font-normal text-xs font-Nunito uppercase text-pink cursor-pointer hover:text-light-pink" />
 			</div>
 		</section>
