e00ba1dde8f7b3562a25cbe72f8993c26389e6c4
Magda <>
2023-08-24 21:09:48 -0400

    stripe setup

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { useSelector } from 'react-redux';
 import { selectTotal, selectItems } from '../store/cartSllice';
 import CartItem from '../components/cartItem';
+//import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+//import { checkout } from '../../../routes/orders';
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 function Cart({ setCart, cart }) {
 	const total = useSelector(selectTotal);
 	const basketItems = useSelector(selectItems);
+	const [ clientSecret, setClientSecret ] = useState('');
+
+	const createCheckoutSession = async () => {
+		console.log(stripePromise);
+		const stripe = await stripePromise;
+		try {
+			const response = await fetch('http://localhost:3001/create-checkout-session', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ basketItems })
+			});
+
+			const checkoutSession = await response.json();
+			const { sessionId } = checkoutSession;
+			const result = await stripe.redirectToCheckout({
+				sessionId: sessionId
+			})
+			if(result.error) {
+				alert(result.error.message)
+			}
+			//const data = await response.json();
+			//setClientSecret(data.clientSecret);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<section
-			className={`w-[40%] h-screen bg-white p-10 flex flex-col fixed transition-all duration-500 ease-in-out ${cart
+			className={`w-[40%] h-screen bg-white p-10 flex flex-col fixed transition-right duration-100 ease-in-out ${cart
 				? 'right-0 '
 				: '-right-full'} `}
 		>
@@ -31,11 +65,14 @@ function Cart({ setCart, cart }) {
 						<h2 className="font-bold text-sm font-Nunito uppercase text-dark-blue">Price</h2>
 						<h2 />
 					</div>
-					<div >{basketItems.map((item) => <CartItem item={item} key={item.id}/>)}</div>
+					<div>{basketItems.map((item) => <CartItem item={item} key={item.id} />)}</div>
 					<div className="flex items-center justify-between border-t-2 border-pink py-4">
 						<h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">Total:</h2>
 						<h2 className="font-bold text-md font-Nunito uppercase text-dark-blue">${total}</h2>
 					</div>
+					
+						<button onClick={createCheckoutSession} type="submit" className="font-bold text-base font-Nunito py-4 px-4 bg-pink uppercase text-white rounded-[10px] hover:bg-light-pink">Checkout</button>
+				
 				</div>
 			)}
 		</section>
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 const mongoosePath = process.env.MONGO_URL;
 mongoose.set('strictQuery', false);
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 mongoose
 	.connect(mongoosePath, {
@@ -32,5 +33,51 @@ app.use(bodyParser.json());
 
 app.use(OrdersRoute);
 app.use(UsersRoute);
+
+app.post('/create-checkout-session', async (req, res) => {
+	console.log(req.body.basketItems);
+	const { basketItems } = req.body;
+	console.log('basket', basketItems);
+	const transformData = basketItems.map((item) => ({
+		price_data: {
+			currency: 'usd',
+			product_data: {
+				name: item.program
+			},
+			unit_amount: item.price * 100
+		},
+		quantity: 1
+	}));
+	
+	try {
+		console.log('transform data', transformData);
+		const session = await stripe.checkout.sessions.create({
+			line_items: transformData,
+			mode: 'payment',
+			success_url: 'http://localhost:4242/success',
+			cancel_url: 'http://localhost:4242/cancel'
+		});
+		res.status(200).json({ sessionId: session.id });
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+app.post('/webhook', bodyParser.raw({ type: 'application/json' }), (req, res) => {
+	const event = req.body;
+
+	// Handle relevant event (e.g., payment_intent.succeeded)
+	switch (event.type) {
+		case 'payment_intent.succeeded':
+			const paymentIntent = event.data.object;
+			// Update your database or perform other actions
+			console.log('Payment succeeded:', paymentIntent.id);
+			break;
+		default:
+			console.log('Unhandled event type:', event.type);
+	}
+
+	res.status(200).send();
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
